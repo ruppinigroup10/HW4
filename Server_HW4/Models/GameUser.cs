b0592a5a34ef33663bdf5e140de7501899b9e480
sharonref <sharonag15@gmail.com>
2025-01-22 16:26:56 +0200

    Update GameUser.cs

--- Server_HW4/Models/GameUser.cs
+++ Server_HW4/Models/GameUser.cs
@@ -17,5 +17,10 @@ namespace Server_HW4.Models
             this.game = game;
             this.user = user;
         }
+        
+        public void hey()
+        {
+            var hey = 0;
+        }
     }
 }
